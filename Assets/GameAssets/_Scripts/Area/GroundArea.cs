e157325ba0da4331cbd8f6b29d1aea75679fec06
Saix_93 <r.torresgonzalez.93@gmail.com>
2018-06-09 05:34:54 +0200

    Añade shaders y funcionalidad para añadir un borde a los meshes

--- Assets/GameAssets/_Scripts/Area/GroundArea.cs
+++ Assets/GameAssets/_Scripts/Area/GroundArea.cs
@@ -51,22 +51,4 @@ public class GroundArea : MonoBehaviour
             rend.material.color = selectedColor;
         }
     }
-
-    private void OnDrawGizmos()
-    {
-        Renderer renderer = GetComponent<Renderer>();
-
-        drawString(props.name, renderer.bounds.center);
-    }
-
-    static void drawString(string text, Vector3 worldPos, Color? colour = null)
-    {
-        UnityEditor.Handles.BeginGUI();
-        if (colour.HasValue) GUI.color = colour.Value;
-        var view = UnityEditor.SceneView.currentDrawingSceneView;
-        Vector3 screenPos = view.camera.WorldToScreenPoint(worldPos);
-        Vector2 size = GUI.skin.label.CalcSize(new GUIContent(text));
-        GUI.Label(new Rect(screenPos.x - (size.x / 2), -screenPos.y + view.position.height + 4, size.x, size.y), text);
-        UnityEditor.Handles.EndGUI();
-    }
 }
